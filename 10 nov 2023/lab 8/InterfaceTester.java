1b00c00ad9f7ebed91c7400fade0d186b7c0c8b5
CEN1TIPEDE <147383021+CEN1TIPEDE@users.noreply.github.com>
2023-11-17 10:50:31 +0700

    second commit : lab 7 and 8 are done.

--- 10 nov 2023/lab 8/InterfaceTester.java
+++ 10 nov 2023/lab 8/InterfaceTester.java
@@ -2,8 +2,8 @@ public class InterfaceTester {
     public static void main(String args[]){
         System.out.println(Circle2.color);
         Circle2.color = "green";
-        Circle2 c2 = new Circle2();
-        System.out.println(c2.color);
+        Circle c2 = new Circle();
+        System.out.println(c2.color); // color cannot changebecause I've set the color as a final attribute. 
         c2.setRadius(3);
         System.out.println(c2.getRadius());
         System.out.println(c2.getArea());
